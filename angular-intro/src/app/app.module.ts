e8f24e5406386f55394a2deaeb57663403472284
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-07 16:51:27 +0530

    greeter component creation

--- angular-intro/src/app/app.module.ts
+++ angular-intro/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GreeterComponent } from '../greeter/greeter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreeterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GreeterComponent]
 })
 export class AppModule { }
