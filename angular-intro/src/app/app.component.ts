abad6a7136f8bf3f42b51d182ea300701b34c2b2
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-07 16:42:37 +0530

    app setup

---
+++ angular-intro/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-intro';
+
+  constructor(){
+   /*  setTimeout(() => {
+      this.title = 'Payroll Manager';
+    }, 10000); */
+  }
+}
