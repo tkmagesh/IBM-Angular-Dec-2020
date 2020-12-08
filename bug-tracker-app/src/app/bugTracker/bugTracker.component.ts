4d825bbb6739be3546f6a36856c2d9d0f42565fb
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-08 16:32:21 +0530

    Defining the Bug Model (interface)

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Bug } from './models/Bug';
 
 @Component({
     selector : 'app-bug-tracker',
     templateUrl: './bugTracker.component.html'
 })
 export class BugTrackerComponent{
+    
+    private currentBugId: number = 0;
 
+    bugs : Bug[] = [];
+
+    onAddNewClick(bugName : string){
+        const newBug : Bug = {
+            id : ++this.currentBugId,
+            name : bugName,
+            isClosed : false,
+            createdAt : new Date()
+        };
+        this.bugs.push(newBug);
+    }
 }
