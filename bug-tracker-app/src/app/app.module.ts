4260af540d52a4a92cab7ce98d377ebf08426675
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-10 15:31:26 +0530

    using moment in creating 'elapsed' pipe

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 
 import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
 import { SortPipe } from './bugTracker/pipes/sort.pipe';
+import { ElapsedPipe } from './bugTracker/pipes/elapsed.pipe';
+
+import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
 
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
-import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
+
 
 import { BugStatsComponent } from './bugTracker/components/bugStats.component';
 import { BugEditComponent} from './bugTracker/components/bugEdit.component';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +26,7 @@ import { BugEditComponent} from './bugTracker/components/bugEdit.component';
     , TrimTextPipe
     , SortPipe
     , ClosedCountPipe
+    , ElapsedPipe
   ],
   imports: [
     BrowserModule
