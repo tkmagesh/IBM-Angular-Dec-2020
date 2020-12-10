750bef0fd539b6366d63ca8d5b41e719827c20da
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-10 12:05:46 +0530

    localStorage - assignment

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -17,10 +17,6 @@ export class BugTrackerComponent{
     sortByDesc : boolean = false;
 
     constructor(private bugOpetations : BugOperationsService){
-        this.bugs.push(this.bugOpetations.createNew('Server communication failure'))
-        this.bugs.push(this.bugOpetations.createNew('Application not responding'))
-        this.bugs.push(this.bugOpetations.createNew('User actions not recognized'))
-        this.bugs.push(this.bugOpetations.createNew('Data integrity checks failed'))
     }
 
     onNewBugCreated(newBug : Bug) {
