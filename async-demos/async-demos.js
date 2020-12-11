6e87fccf715bc1711348aec0bf6e732298060cd9
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-11 10:03:49 +0530

    sync to async

--- async-demos/async-demos.js
+++ async-demos/async-demos.js
@@ -14,4 +14,21 @@
 
     window['addSyncClient'] = addSyncClient
 
+    function addAsync(x,y){
+        console.log(`   [@Service] processing ${x} and ${y}`);
+        setTimeout(function(){
+            const result = x + y;
+            console.log(`   [@Service] returning result`);
+            return result;
+        }, 5000);
+    }
+
+    function addAsyncClient(x,y){
+        console.log(`[@Client] triggering the service`);
+        const result = addAsync(x,y);
+        console.log(`[@Client] result = ${result}`);
+    }
+
+    window['addAsyncClient'] = addAsyncClient
+
 })();
